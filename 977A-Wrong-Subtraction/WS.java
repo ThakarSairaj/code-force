6fa171c7a211a7228924b1c14f5fef8ef966b851
sairaj <sairaj.contact@gmail.com>
2025-09-12 20:50:03 +0530

    977A-Wrong-Subtraction

---
+++ 977A-Wrong-Subtraction/WS.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class WS {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in); 
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        
+        int res = 0;
+        
+        
+        for(int i = 0; i < k; i++)
+        {
+            if(n % 10 > 0)
+            {
+                n--;
+            }       
+            else{
+                n /= 10;
+            }
+        }
+        System.out.println(n);
+    }
+}
